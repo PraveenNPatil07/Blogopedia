27b03a0f9435d04775c1c2befab662b43a4ead93
Praveen N Patil <138059710+PraveenNPatil07@users.noreply.github.com>
2024-11-24 10:57:42 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+from api.forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from dotenv import load_dotenv
 import os
 # Optional: add contact me email functionality (Day 60)
